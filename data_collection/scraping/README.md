f83ec0ccd9bfc0ee8942bc10cbce380c912e5aec
mtm_007 <45622655+mtm-007@users.noreply.github.com>
2025-10-02 22:54:34 +0000

    selenium bot google captia caught

---
+++ data_collection/scraping/README.md
@@ -0,0 +1,2 @@
+### Installing chromdriver
+-https://storage.googleapis.com/chrome-for-testing-public/141.0.7390.54/linux64/chromedriver-linux64.zip
---
+++ data_collection/scraping/main.py
@@ -0,0 +1,57 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+import time
+
+
+options = Options()
+options.add_argument("--headless=new")   # run without a GUI
+options.add_argument("--no-sandbox")     # needed inside containers
+options.add_argument("--disable-dev-shm-usage")  # avoid limited /dev/shm space
+
+options.add_argument("--disable-blink-features=AutomationControlled")  # Hide automation
+options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
+options.add_argument("--window-size=1920,1080")
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option('useAutomationExtension', False)
+
+
+
+service = Service(executable_path='chromedriver-linux64/chromedriver')
+driver = webdriver.Chrome(service=service, options=options)
+
+
+# Hide webdriver property
+# driver.execute_cdp_cmd('Network.setUserAgentOverride', {
+#     "userAgent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+# })
+# driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+
+
+driver.get("https://google.com")
+
+# wait = WebDriverWait(driver, 10)
+# input_element = wait.until(
+#     EC.element_to_be_clickable((By.CLASS_NAME,"gLFyf"))
+# )
+# input_element.click()
+# time.sleep(2)
+# input_element.send_keys("what is the weather here")
+# time.sleep(1.5)
+# input_element.send_keys(Keys.RETURN)
+
+#time.sleep(3)
+input_element = driver.find_element(By.CLASS_NAME, "gLFyf")
+time.sleep(0.5)
+input_element.send_keys("new iphone 17" + Keys.ENTER)
+
+print(f"Title: {driver.title}")
+driver.save_screenshot("result_i.png")
+print("screenshot saved")
+driver.quit()
+print("driver quit run")
